016a9f172d395ed5d06b55968a3b2d128f302b85
Serin <serin0837@yahoo.com>
2021-01-07 18:08:29 +0000

    started study react again

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person'// start with Uppercase and import name does not really matter
 
 function App() {
   return (
     <div className="App">
-
+      <h1> Hi I am a react app</h1> 
+      <h2> Serin</h2>
+      <Person/>
     </div>
   );
 }
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from "react"
+// ex6 function
+const person = () => {
+    return <p>I am a person</p>
+}
+
+export default person 
+
+
